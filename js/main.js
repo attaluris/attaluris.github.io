632abfc0c2685a25a30c06a8e8f41b0187c94772
attaluris <sritej@gmail.com>
2018-06-26 20:27:39 -0700

    added social_links

--- js/main.js
+++ js/main.js
@@ -3,11 +3,11 @@ var image_tracker = 'one';
 function change() {
     var image = document.getElementById('profile');
     if (image_tracker == 'one') {
-        image.src = 'two.jpg';
+        image.src = 'assets/two.jpg';
         pic_caption.innerHTML = '(flower me)';
         image_tracker = 'two';
     } else {
-        image.src = 'one.jpg';
+        image.src = 'assets/one.jpg';
         pic_caption.innerHTML = '(business me)';
         image_tracker = 'one';
     }
